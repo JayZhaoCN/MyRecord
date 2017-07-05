4ac57befa46764c81581b90f732564eb4c5fb3f5
zhaojiabao <zhaojiabao@smartdevices.com.cn>
2017-07-05 16:27:56 +0800

    feat |> 添加一个loadingDialog

---
+++ app/src/main/java/com/hfut/zhaojiabao/myrecord/dialogs/JayLoadingDialog.java
@@ -0,0 +1,138 @@
+package com.hfut.zhaojiabao.myrecord.dialogs;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.hfut.zhaojiabao.myrecord.R;
+
+/**
+ * @author zhaojiabao 2017/7/5
+ */
+
+public class JayLoadingDialog extends DialogFragment {
+    private static final int STATE_LOADING = 0;
+    private static final int STATE_ERROR = 1;
+    private static final int STATE_SUCCESS = 2;
+
+    private int mState = STATE_LOADING;
+
+    private ProgressBar mLoadingPb;
+    private TextView mLoadingTipsTv;
+    private ImageView mLoadingImg;
+
+    private String mLoadingTips = "";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.dialog_jay_loading, container, false);
+        initViews(v);
+        updateViews();
+        return v;
+    }
+
+    private void initViews(View v) {
+        mLoadingPb = (ProgressBar) v.findViewById(R.id.loading_pb);
+        mLoadingTipsTv = (TextView) v.findViewById(R.id.loading_tips_tv);
+        mLoadingImg = (ImageView) v.findViewById(R.id.loading_img);
+    }
+
+    private void updateViews() {
+        mLoadingTipsTv.setText(mLoadingTips);
+        switch (mState) {
+            case STATE_ERROR:
+                showErrorInternal();
+                break;
+            case STATE_LOADING:
+                showLoadingInternal();
+                break;
+            case STATE_SUCCESS:
+                showSuccessInternal();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void setLoadingTips(String tips) {
+        mLoadingTips = tips;
+        if (mLoadingTipsTv != null) {
+            mLoadingTipsTv.setText(mLoadingTips);
+        }
+    }
+
+    public void setLoadingTips(@StringRes int tipsRes) {
+        mLoadingTips = getString(tipsRes);
+        if (mLoadingTipsTv != null) {
+            mLoadingTipsTv.setText(mLoadingTips);
+        }
+    }
+
+    private void showErrorInternal() {
+        mLoadingTipsTv.setText(mLoadingTips);
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.VISIBLE);
+        mLoadingPb.setVisibility(View.GONE);
+        mLoadingImg.setImageResource(R.drawable.loading_error);
+    }
+
+    public void showError(String errorTips) {
+        setLoadingTips(errorTips);
+        mState = STATE_ERROR;
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.VISIBLE);
+        mLoadingPb.setVisibility(View.GONE);
+        mLoadingImg.setImageResource(R.drawable.loading_error);
+    }
+
+    private void showLoadingInternal() {
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.GONE);
+        mLoadingPb.setVisibility(View.VISIBLE);
+    }
+
+    public void showLoading(String loadingTips) {
+        setLoadingTips(loadingTips);
+        mState = STATE_LOADING;
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.GONE);
+        mLoadingPb.setVisibility(View.VISIBLE);
+    }
+
+    private void showSuccessInternal() {
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.VISIBLE);
+        mLoadingPb.setVisibility(View.GONE);
+        mLoadingImg.setImageResource(R.drawable.loading_done);
+    }
+
+    public void showSuccess(String successTips) {
+        setLoadingTips(successTips);
+        mState = STATE_SUCCESS;
+        if (mLoadingImg == null || mLoadingPb == null) {
+            return;
+        }
+        mLoadingImg.setVisibility(View.VISIBLE);
+        mLoadingPb.setVisibility(View.GONE);
+        mLoadingImg.setImageResource(R.drawable.loading_done);
+    }
+}
