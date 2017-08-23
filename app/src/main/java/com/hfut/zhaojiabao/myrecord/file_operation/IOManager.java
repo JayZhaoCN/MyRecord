68a2a60a8d1ce6ba39fba4ff168350b89d99f939
zhaojiabao <zhaojiabao@smartdevices.com.cn>
2017-08-23 15:54:27 +0800

    fix |> IOManager some optimize

--- app/src/main/java/com/hfut/zhaojiabao/myrecord/file_operation/IOManager.java
+++ app/src/main/java/com/hfut/zhaojiabao/myrecord/file_operation/IOManager.java
@@ -62,44 +62,7 @@ public class IOManager {
             @Override
             public void call(Subscriber<? super String> subscriber) {
                 try {
-                    File file = new File(filePath);
-                    if (!file.getParentFile().exists()) {
-                        Log.i(TAG, "parents not exist, so create: " + file.getParentFile().mkdir());
-                    }
-                    if (!file.exists()) {
-                        Log.i(TAG, "file not exist, so create: " + file.createNewFile());
-                    }
-                    FileWriter fileWriter = new FileWriter(file, true);
-                    BufferedWriter writer = new BufferedWriter(fileWriter);
-
-                    //write record to sdcard
-                    List<Record> records = JayDaoManager.getInstance().getDaoSession().getRecordDao().loadAll();
-                    for (Record record : records) {
-                        writer.write(record.toJSONString());
-                        writer.newLine();
-                    }
-
-                    //write category data to sdcard
-                    //先写入分割标志
-                    writer.write(FILE_DIVIDER);
-                    writer.newLine();
-                    List<Category> categories = JayDaoManager.getInstance().getDaoSession().getCategoryDao().loadAll();
-                    for (Category category : categories) {
-                        writer.write(category.toJSONString());
-                        writer.newLine();
-                    }
-
-                    //write user name data to sdcard
-                    //先写入分割标志
-                    writer.write(FILE_DIVIDER);
-                    writer.newLine();
-                    List<User> users = JayDaoManager.getInstance().getDaoSession().getUserDao().loadAll();
-                    writer.write(users.get(0).toJSONString());
-                    writer.newLine();
-
-                    writer.close();
-                    fileWriter.close();
-
+                    backupFile(filePath);
                     subscriber.onCompleted();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -137,7 +100,51 @@ public class IOManager {
 
                     }
                 });
+    }
 
+    /**
+     * backup file
+     * SYNC
+     * throws exception
+     */
+    private static void backupFile(String filePath) throws Exception {
+        File file = new File(filePath);
+        if (!file.getParentFile().exists()) {
+            Log.i(TAG, "parents not exist, so create: " + file.getParentFile().mkdir());
+        }
+        if (!file.exists()) {
+            Log.i(TAG, "file not exist, so create: " + file.createNewFile());
+        }
+        FileWriter fileWriter = new FileWriter(file, true);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+
+        //write record to sdcard
+        List<Record> records = JayDaoManager.getInstance().getDaoSession().getRecordDao().loadAll();
+        for (Record record : records) {
+            writer.write(record.toJSONString());
+            writer.newLine();
+        }
+
+        //write category data to sdcard
+        //先写入分割标志
+        writer.write(FILE_DIVIDER);
+        writer.newLine();
+        List<Category> categories = JayDaoManager.getInstance().getDaoSession().getCategoryDao().loadAll();
+        for (Category category : categories) {
+            writer.write(category.toJSONString());
+            writer.newLine();
+        }
+
+        //write user name data to sdcard
+        //先写入分割标志
+        writer.write(FILE_DIVIDER);
+        writer.newLine();
+        List<User> users = JayDaoManager.getInstance().getDaoSession().getUserDao().loadAll();
+        writer.write(users.get(0).toJSONString());
+        writer.newLine();
+
+        writer.close();
+        fileWriter.close();
     }
 
     public static void verifyStoragePermissions(Context context) {
