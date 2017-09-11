ea62582e7776336f6141f664b2c6614330a73ef1
zhaojiabao <zhaojiabao@smartdevices.com.cn>
2017-09-11 17:24:38 +0800

    修复TypefaceTextView 不设置fontFamily导致的异常

--- app/src/main/java/com/hfut/zhaojiabao/myrecord/typeface/TypefaceTextView.java
+++ app/src/main/java/com/hfut/zhaojiabao/myrecord/typeface/TypefaceTextView.java
@@ -3,7 +3,9 @@ package com.hfut.zhaojiabao.myrecord.typeface;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 
 /**
  * @author zhaojiabao 2017/6/22
@@ -11,6 +13,7 @@ import android.util.AttributeSet;
  */
 
 public class TypefaceTextView extends AppCompatTextView {
+    private static final String TAG = "TypefaceTextView";
     public TypefaceTextView(Context context) {
         this(context, null);
     }
@@ -23,7 +26,11 @@ public class TypefaceTextView extends AppCompatTextView {
         super(context, attrs, defStyleAttr);
         TypedArray ta = context.obtainStyledAttributes(attrs, new int[] {android.R.attr.fontFamily});
         String fontFamily = ta.getString(0);
-        FontManager.applyTypeface(context, this, fontFamily);
+        if (TextUtils.isEmpty(fontFamily)) {
+            Log.w(TAG, "you should add android:fontFamily attribute in your layout file!");
+        } else {
+            FontManager.applyTypeface(context, this, fontFamily);
+        }
         ta.recycle();
     }
 }
